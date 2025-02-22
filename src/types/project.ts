69a725b96c83d68c0a0086cc86336a1b7fcefcd7
Aayush Shukla <oreus.aks@gmail.com>
2025-02-22 16:41:15 +0530

    project configurations

---
+++ src/types/project.ts
@@ -0,0 +1,29 @@
+export enum InputType {
+  TEXT = "text",
+  SELECT = "select",
+  CHECKBOX = "checkbox",
+  NUMBER = "number",
+  TEXTAREA = "textarea",
+}
+
+export interface SelectOption {
+  value: string;
+  label: string;
+}
+
+export interface QuestionConfig {
+  id: string;
+  label: string;
+  type: InputType;
+  options?: SelectOption[];
+  required: boolean;
+  pattern?: string;
+  patternError?: string;
+  description?: string;
+  defaultValue?: string | boolean | number;
+}
+
+export interface ProjectConfig {
+  projectType: string;
+  answers: Record<string, any>;
+}
