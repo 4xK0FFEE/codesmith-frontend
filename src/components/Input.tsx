5f700136f227d3dd8b24a6bf447a2dab44baf0fc
Aayush Shukla <oreus.aks@gmail.com>
2025-02-22 16:43:28 +0530

    change style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,3 @@
-// import React from "react";
-
-// const Input = () => {
-//   return <div>Input</div>;
-// };
-
-// export default Input;
-
 import React, { ChangeEvent } from "react";
 import { cn } from "@/utils/cn";
 
@@ -18,17 +10,16 @@ interface InputProps {
 
 const Input = ({ value, onChange, placeholder, className }: InputProps) => {
   return (
-    <div className={cn("rounded-full", className)}>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className={cn(
-          "px-6 py-3 text-2xl text-cosmic-text rounded-full border-2 border-cosmic-text bg-inkpaper-text"
-        )}
-      />
-    </div>
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className={cn(
+        "rounded-xl px-4 py-2 text-cosmic-text border-2 border-cosmic-text",
+        className
+      )}
+    />
   );
 };
 
